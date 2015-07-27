4e4efb5f1acceb0f878bd18b286ee36e20dcd522
Benoît Laniel <nels+git@pgroupe.net>
2015-07-27 19:51:15 +0200

    Use opensource pvr_drv_video
    Change-Id: I9768763c7d0423872b83b0a885c5cf4528386474

--- p52xx-vendor-blobs.mk
+++ p52xx-vendor-blobs.mk
@@ -31,7 +31,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/p52xx/common/system/lib/libsepdrm.so:system/lib/libsepdrm.so \
     vendor/samsung/p52xx/common/system/lib/libmultidisplay.so:system/lib/libmultidisplay.so \
     vendor/samsung/p52xx/common/system/lib/libmultidisplayjni.so:system/lib/libmultidisplayjni.so \
-    vendor/samsung/p52xx/common/system/lib/pvr_drv_video.so:system/lib/pvr_drv_video.so \
     vendor/samsung/p52xx/common/system/bin/geomagneticd:system/bin/geomagneticd \
     vendor/samsung/p52xx/common/system/bin/orientationd:system/bin/orientationd \
     vendor/samsung/p52xx/common/system/lib/libia_aiq.so:system/lib/libia_aiq.so \
